86284de7f4eae45a7856fe4bc83bd1c8d8e19742
Simad9 <wijdanbita@gmail.com>
2023-12-09 10:56:56 +0700

    testing hasing

--- main.cpp
+++ main.cpp
@@ -1,25 +1,66 @@
 #include <iostream>
+#include <unordered_map>
 using namespace std;
 
 // DEKLARASI
 // ...
 
 // STRUCT
-// ...
+struct Data {
+  string namaProduk;
+  int harga;
+  int kodeProduk;
+};
+// Data ada 10
+Data produk[10];
 
 // FUNGSI LOGIKA (BACK-END)
-// ...
+// Fungsi hash sederhana untuk menghasilkan nilai hash dari string (sting nama produk)
+int simpleHash(const string& input) {
+  int hash = 0;
+  for (char c : input) {
+    hash = hash * 31 + c;
+  }
+  return hash;
+}
 
 // FUNGSI TAMPILAN (FRONT-END)
-// ...
+// Kurang tau fungsinya
+unordered_map<int, string> hashTable;
+// output input
+void inputData() {
+  // Testing doang ini
+  cout << "Masukan Produk : " << endl;
+  cout << "Nama : ";
+  getline(cin >> ws, produk->namaProduk);
+  cout << "Harga : ";
+  cin >> produk->harga;
+
+  // Untung hasing dari namanya
+  int hashValue = simpleHash(produk->namaProduk);
+  hashTable[hashValue] = produk->namaProduk;
+}
+
+void outputData() {
+  // Nampilin doang, buat testing
+  cout << "Tampilan Produk" << endl;
+  cout << "Nama : " << produk->namaProduk << endl;
+  cout << "Harga : " << produk->harga << endl;
+  //  --- hasingnya ---
+  // Output data
+  cout << "\nData yang dimasukkan:" << endl;
+  for (const auto& entry : hashTable) {
+    cout << "Hash: " << entry.first << ", Nilai: " << entry.second << endl;
+  }
+  // Biar gak ilang, soalnya cuman coba aja
+  system("pause");
+}
 
 // MAIN PROGRAM
-int main()
-{
+int main() {
   // Deklarasi Variable
   int pilihMenu;
-  do
-  {
+  do {
     // Tampilan Awal
     system("cls");
     cout << "+==================================+\n";
@@ -27,6 +68,8 @@ int main()
     cout << "|            1232200019            |\n";
     cout << "|            1232200021            |\n";
     cout << "|            1232200031            |\n";
+    cout << "+==================================+\n";
+    cout << "|          Toko Serba Ada          |\n";
     cout << "+==================================+\n";
     cout << "| 1. Input Data                    |\n";
     cout << "| 2. Output Data                   |\n";
@@ -41,13 +84,25 @@ int main()
     cin >> pilihMenu;
 
     // Error Handling Input Menu
-    if (cin.fail() || pilihMenu < 1 || pilihMenu > 6)
-    {
+    if (cin.fail() || pilihMenu < 1 || pilihMenu > 6) {
       cout << "  Inputan tidak terdeteksi!\n  ";
       cin.clear();
       cin.ignore();
       system("pause");
       continue;
+    }
+
+    if (pilihMenu == 1) {  // 1. Input Data
+      inputData();
+    } else if (pilihMenu == 2) {  // 2. Ouput Data
+      outputData();
+    } else if (pilihMenu == 3) {  // 3. Search Data
+
+    } else if (pilihMenu == 4) {  // 4. Sort Data
+
+    } else if (pilihMenu == 5) {  // 5. Delete Data
+
+    } else if (pilihMenu == 6) {  // 6. Delete History
     }
 
   } while (true);
